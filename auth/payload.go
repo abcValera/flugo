2508a61c8207c5ccafd07ba01f10af05d5afb8d0
abcValera <zoocity14@gmail.com>
2023-02-23 00:05:59 +0200

    Add: payload model for JWT

---
+++ auth/payload.go
@@ -0,0 +1,47 @@
+package auth
+
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+var (
+	ErrExpiredToken = errors.New("token has expired")
+	ErrInvalidToken = errors.New("invalid token")
+)
+
+type Payload struct {
+	ID        uuid.UUID `json:"id"`
+	UserID    int32     `json:"user_id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	IssuedAt  time.Time `json:"issued_at"`
+	ExpiredAt time.Time `json:"expired_at"`
+}
+
+func NewPayload(UserID int32, username, email string, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	payload := &Payload{
+		ID:        tokenID,
+		UserID:    UserID,
+		Username:  username,
+		Email:     email,
+		IssuedAt:  time.Now(),
+		ExpiredAt: time.Now().Add(duration),
+	}
+
+	return payload, nil
+}
+
+func (p *Payload) Valid() error {
+	if time.Now().After(p.ExpiredAt) {
+		return ErrExpiredToken
+	}
+	return nil
+}
