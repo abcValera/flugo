3d39ce2b74bffa061a225d68934335b8c3a0b93f
abcValera <zoocity14@gmail.com>
2023-02-24 16:50:11 +0200

    Fix: auth vars are public now

--- middleware/jwt_middleware.go
+++ middleware/jwt_middleware.go
@@ -1,4 +1,4 @@
-package server
+package middleware
 
 import (
 	"net/http"
@@ -9,37 +9,35 @@ import (
 )
 
 const (
-	authHeaderKey  = "authorization"
-	authTypeBearer = "bearer"
-	authPayloadKey = "auth_payload"
+	AuthHeaderKey  = "authorization"
+	AuthTypeBearer = "bearer"
+	AuthPayloadKey = "auth_payload"
 )
 
-func NewAuthMiddleware(tokenMaker token.Maker) func(next fiber.Handler) fiber.Handler {
-	return func(next fiber.Handler) fiber.Handler {
-		return func(c *fiber.Ctx) error {
-			authHeader := c.Get(authHeaderKey)
-			if len(authHeader) == 0 {
-				return fiber.NewError(fiber.StatusUnauthorized, "authorization is not provided")
-			}
-
-			fields := strings.Fields(authHeader)
-			if len(fields) < 2 {
-				return fiber.NewError(http.StatusUnauthorized, "invalid authorization")
-			}
-
-			authType := strings.ToLower(fields[0])
-			if authType != authTypeBearer {
-				return fiber.NewError(http.StatusUnauthorized, "this authorization type is not supported")
-			}
-
-			accessToken := fields[1]
-			payload, err := tokenMaker.VerifyToken(accessToken)
-			if err != nil {
-				return fiber.NewError(http.StatusUnauthorized, err.Error())
-			}
-			c.Locals(authPayloadKey, payload)
-
-			return next(c)
+func NewAuthMiddleware(tokenMaker token.Maker) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		authHeader := c.Get(AuthHeaderKey)
+		if len(authHeader) == 0 {
+			return fiber.NewError(fiber.StatusUnauthorized, "authorization is not provided")
 		}
+
+		fields := strings.Fields(authHeader)
+		if len(fields) < 2 {
+			return fiber.NewError(http.StatusUnauthorized, "invalid authorization")
+		}
+
+		authType := strings.ToLower(fields[0])
+		if authType != AuthTypeBearer {
+			return fiber.NewError(http.StatusUnauthorized, "this authorization type is not supported")
+		}
+
+		accessToken := fields[1]
+		payload, err := tokenMaker.VerifyToken(accessToken)
+		if err != nil {
+			return fiber.NewError(http.StatusUnauthorized, err.Error())
+		}
+		c.Locals(AuthPayloadKey, payload)
+
+		return c.Next()
 	}
 }
